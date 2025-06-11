c1a5c09ee4fa65f015b1a8d79b34efd70b2517a5
subhajit <subhajitbhattacharya08@gmail.com>
2025-06-11 09:24:50 +0530

    Second Commit-added Route Handler

--- src/app.js
+++ src/app.js
@@ -3,8 +3,21 @@ const express = require('express');
 const app = express(); //create an instance of express
 const port = 3000;
 
-app.use("/hello", (req, res) => {
-  res.send('Hello from the server!');})
+app.use("/user",
+  (req, res, next) => {
+    console.log("Middleware for /user route");
+    next(); // Call the next middleware or route handler
+  },
+  (req, res, next) => {
+    console.log("Second Handler");
+    next();
+    // res.send("User route accessed");
+  },
+  (req, res) => {
+    res.send("Sending response from third block of middleware");
+  }
+)
+
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
